ac14d34a6d87e353fbf6ce769ce2186086d98b95
Nikki <nikki.satopdx@gmail.com>
2021-03-15 14:19:28 -0700

    finished checkoutpage

--- src/Product.js
+++ src/Product.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import './Product.css';
-function Product({ title, image, price, rating }) {
+import { useStateValue } from './StateProvider';
+
+function Product({ id, title, image, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
+
+  const addToBasket = () => {
+    dispatch({
+      type: 'ADD_TO_BASKET',
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
   return (
     <div className='product'>
       <div className='product__info'>
@@ -13,14 +29,14 @@ function Product({ title, image, price, rating }) {
           {Array(rating)
             .fill()
             .map((_, i) => (
-              <p>⭐️</p>
+              <p key={i}>⭐️</p>
             ))}
         </div>
       </div>
 
       <img src={image} alt='product' />
 
-      <button>Add to Basket</button>
+      <button onClick={addToBasket}>Add to Basket</button>
     </div>
   );
 }
